7030fbb4603bde5b2d777f338e89874c8a2972fd
vnsoft <vnsoft.he@gmail.com>
2016-03-25 16:36:22 +0800

    Added files via upload

---
+++ __openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'web_change_title_odoo',
+    'version': '0.1',
+    'category': 'Hidden',
+    'description': """
+Web Change Title for Odoo.
+================
+Please insert a record to parameter,and key='company.title',value='new title'.
+""",
+    'author': 'VnSoft',
+    'website': 'http://blog.csdn.net/vnsoft',
+    'depends': ['web'],
+    'data':['web.xml'],
+    'installable': True,
+    'auto_install': False,
+}
